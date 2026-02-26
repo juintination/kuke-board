f1208580c049e778233ae4c227c7d577daf9d59b
DEOKJAE KWON <juintin@kakao.com>
2026-02-26 22:40:05 +0900

    feat: 인덱스 추가

--- service/article/src/main/kotlin/kuke/board/article/entity/Article.kt
+++ service/article/src/main/kotlin/kuke/board/article/entity/Article.kt
@@ -7,7 +7,15 @@ import org.hibernate.annotations.SQLDelete
 import org.hibernate.annotations.SQLRestriction
 
 @Entity
-@Table(name = "article")
+@Table(
+    name = "article",
+    indexes = [
+        Index(
+            name = "idx_article_board_id_deleted_at_id",
+            columnList = "board_id, deleted_at, id"
+        )
+    ]
+)
 @SQLRestriction("deleted_at is null")
 @SQLDelete(sql = "update article set deleted_at = now() where id = ?")
 class Article private constructor(
