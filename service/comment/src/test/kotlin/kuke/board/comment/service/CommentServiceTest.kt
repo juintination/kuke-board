1c6dfed113824932817c982ab601f29a52e6f640
DEOKJAE KWON <juintin@kakao.com>
2026-03-04 15:07:18 +0900

    test: 테스트 코드 수정

--- service/comment/src/test/kotlin/kuke/board/comment/service/CommentServiceTest.kt
+++ service/comment/src/test/kotlin/kuke/board/comment/service/CommentServiceTest.kt
@@ -5,15 +5,32 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.mockk.*
 import kuke.board.comment.entity.Comment
 import kuke.board.comment.repository.CommentRepository
+import kuke.board.common.snowflake.Snowflake
 import org.springframework.data.repository.findByIdOrNull
 
-class CommentServiceDeleteTest : BehaviorSpec({
+class CommentServiceTest : BehaviorSpec({
 
+    val snowflake = Snowflake()
     val commentRepository = mockk<CommentRepository>()
 
     val commentService = CommentService(
         commentRepository = commentRepository,
     )
+
+    fun commentFixture(
+        id: Long = snowflake.nextId(),
+        parentId: Long? = null,
+        tombstoned: Boolean = false,
+    ): Comment {
+        val comment = mockk<Comment>()
+
+        every { comment.id } returns id
+        every { comment.parentId } returns parentId
+        every { comment.isTombstoned() } returns tombstoned
+        every { comment.tombstone() } just Runs
+
+        return comment
+    }
 
     afterTest {
         clearAllMocks()
@@ -24,11 +41,12 @@ class CommentServiceDeleteTest : BehaviorSpec({
         When("삭제 대상 댓글에 자식(대댓글)이 있으면") {
             Then("tombstone 처리만 하고 삭제 처리는 하지 않는다") {
                 val commentId = 10L
-                val comment = mockk<Comment>()
+                val comment = commentFixture(
+                    id = commentId
+                )
 
                 every { commentRepository.findByIdOrNull(commentId) } returns comment
                 every { commentRepository.existsByParentId(commentId) } returns true
-                every { comment.tombstone() } just Runs
 
                 commentService.delete(commentId)
 
@@ -46,12 +64,14 @@ class CommentServiceDeleteTest : BehaviorSpec({
         When("삭제 대상 댓글에 자식이 없고, root 댓글이면") {
             Then("해당 댓글을 삭제 처리하고 종료한다") {
                 val commentId = 10L
-                val comment = mockk<Comment>()
+                val comment = commentFixture(
+                    id = commentId,
+                    parentId = null
+                )
 
                 every { commentRepository.findByIdOrNull(commentId) } returns comment
                 every { commentRepository.existsByParentId(commentId) } returns false
                 every { commentRepository.delete(comment) } just Runs
-                every { comment.parentId } returns null
 
                 commentService.delete(commentId)
 
@@ -71,16 +91,20 @@ class CommentServiceDeleteTest : BehaviorSpec({
                 val commentId = 10L
                 val parentId = 9L
 
-                val child = mockk<Comment>()
-                val parent = mockk<Comment>()
+                val child = commentFixture(
+                    id = commentId,
+                    parentId = parentId
+                )
+                val parent = commentFixture(
+                    id = parentId,
+                    tombstoned = false
+                )
 
                 every { commentRepository.findByIdOrNull(commentId) } returns child
                 every { commentRepository.existsByParentId(commentId) } returns false
                 every { commentRepository.delete(child) } just Runs
 
-                every { child.parentId } returns parentId
                 every { commentRepository.findByIdOrNull(parentId) } returns parent
-                every { parent.isTombstoned() } returns false
 
                 commentService.delete(commentId)
 
@@ -102,17 +126,20 @@ class CommentServiceDeleteTest : BehaviorSpec({
                 val commentId = 10L
                 val parentId = 9L
 
-                val child = mockk<Comment>()
-                val parent = mockk<Comment>()
+                val child = commentFixture(
+                    id = commentId,
+                    parentId = parentId
+                )
+                val parent = commentFixture(
+                    id = parentId,
+                    tombstoned = true
+                )
 
                 every { commentRepository.findByIdOrNull(commentId) } returns child
                 every { commentRepository.existsByParentId(commentId) } returns false
                 every { commentRepository.delete(child) } just Runs
 
-                every { child.parentId } returns parentId
                 every { commentRepository.findByIdOrNull(parentId) } returns parent
-                every { parent.isTombstoned() } returns true
-                every { parent.id } returns parentId
                 every { commentRepository.existsByParentId(parentId) } returns true
 
                 commentService.delete(commentId)
@@ -137,23 +164,23 @@ class CommentServiceDeleteTest : BehaviorSpec({
                 val commentId = 10L
                 val parentId = 9L
 
-                val child = mockk<Comment>()
-                val parent = mockk<Comment>()
+                val child = commentFixture(
+                    id = commentId,
+                    parentId = parentId
+                )
+                val parent = commentFixture(
+                    id = parentId,
+                    parentId = null,
+                    tombstoned = true
+                )
 
                 every { commentRepository.findByIdOrNull(commentId) } returns child
                 every { commentRepository.existsByParentId(commentId) } returns false
                 every { commentRepository.delete(child) } just Runs
 
-                every { child.parentId } returns parentId
-
                 every { commentRepository.findByIdOrNull(parentId) } returns parent
-                every { parent.isTombstoned() } returns true
-                every { parent.id } returns parentId
                 every { commentRepository.existsByParentId(parentId) } returns false
                 every { commentRepository.delete(parent) } just Runs
-
-                // 재귀 종료 조건: parent가 root
-                every { parent.parentId } returns null
 
                 commentService.delete(commentId)
 
