d5133b27c81300a0f7ff921ffa06b45e1f9642c4
DEOKJAE KWON <juintin@kakao.com>
2026-04-06 19:45:19 +0900

    feat: Spring Cloud Gateway 기반 서비스 라우팅 구현

---
+++ service/gateway/src/main/kotlin/kuke/board/gateway/GatewayApplication.kt
@@ -0,0 +1,11 @@
+package kuke.board.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<GatewayApplication>(*args)
+}
